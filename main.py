302a72a87c8ae21d35eb5e49689b2f6e90655677
c00lhawk607 <jordandixon2004@outlook.com>
2021-07-21 15:42:29 -0500

    updating port

--- main.py
+++ main.py
@@ -31,5 +31,6 @@ def home3():
 #####################################################
 #                  Runs the website                 #
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    import os
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
 #####################################################

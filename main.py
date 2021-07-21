a62ec3f7475c5adeb06cda8ed603909c6194a118
c00lhawk607 <jordandixon2004@outlook.com>
2021-07-21 14:49:48 -0500

    updating procfile

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ def home3():
 #####################################################
 #                  Runs the website                 #
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    app.run()
 #####################################################
